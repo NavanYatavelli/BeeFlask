5fb2f41e798d640bfcf9bdca411f4112b0e1dd5d
navan <navan.reddy@gmail.com>
2022-09-25 11:06:54 -0700

    Bootstrap Page Built

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ def index():
 def stub():
     return render_template("stub.html")
 
+@app.route('/bootstrap/')  # connects /stub/ URL to stub() function
+def bootstrap():
+    return render_template("bootstrap.html")
+
 # this runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
